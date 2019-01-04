b7e735285ff9db76f565e17f8c9a0f59a66ebedd
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-04 03:12:59 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   post 'attractions/:id' => 'attractions#take_ride', as: :take_ride
 
   namespace :admin do
-    resources :attractions, except: [:new, :show]
+    resources :attractions, except: [:new, :show, :edit]
   end
 end
