edf94e25a623954dd857ee1c7c1a7fad93f1f788
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-04 03:32:36 +0000

    Automatically backed up by Learn

--- app/controllers/admin/attractions_controller.rb
+++ app/controllers/admin/attractions_controller.rb
@@ -6,11 +6,4 @@ class Admin::AttractionsController < ApplicationController
   def show
     @attraction = Attraction.find(params[:id])
   end
-
-
-  def update
-    @attraction.update(attraction_params)
-    return edit_admin_attraction_path(@attraction) unless @attraction.save
-    redirect_to admin_attraction_path(@attraction)
-  end
 end
