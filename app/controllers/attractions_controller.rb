8829c82815dfb42cfe48bb8848aadd2ca0b5e216
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2019-01-04 03:20:45 +0000

    Automatically backed up by Learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -7,6 +7,10 @@ class AttractionsController < ApplicationController
     @attraction = Attraction.find(params[:id])
   end
 
+  def new
+    @attraction = Attraction.new
+  end
+
   def create
     @attraction = Attraction.create(attraction_params)
     return redirect_to '/attractions/new' unless @attraction.save
@@ -23,6 +27,6 @@ class AttractionsController < ApplicationController
 
   private
   def attraction_params
-    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)    
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 end
